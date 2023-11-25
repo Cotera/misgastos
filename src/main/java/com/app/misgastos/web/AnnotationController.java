f92e91af3f4ebf405454638ba17235bdff1122f6
Ruben Cotera <cotera.pc.technical@gmail.com>
2023-11-25 15:03:35 +0100

    Corrección de la ruta de repositorio

--- src/main/java/com/app/misgastos/web/AnnotationController.java
+++ src/main/java/com/app/misgastos/web/AnnotationController.java
@@ -1,7 +1,6 @@
 package com.app.misgastos.web;
 
 import com.app.misgastos.model.AnnotacionDto;
-import com.app.misgastos.services.AnnotationService;
 import com.app.misgastos.services.impl.AnnotationServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,23 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.Optional;
 
 @Controller
-@RestController("/annotation")
+@RequestMapping("/annotation")
 public class AnnotationController {
     Logger log = LoggerFactory.getLogger(AnnotationController.class);
 
     @Autowired
     private AnnotationServiceImpl annotationService;
 
-    @PutMapping
+    @PostMapping
     public ResponseEntity<AnnotacionDto> create(@RequestBody AnnotacionDto annotacionDto){
         log.info("Creating object with id: " + annotacionDto.getId());
         annotationService.createAnotation(annotacionDto);
@@ -33,7 +28,7 @@ public class AnnotationController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<AnnotacionDto> getById(@PathParam("id") Long id){
+    public ResponseEntity<AnnotacionDto> getById(@PathVariable("id") Long id){
         log.info("Searching for object with id: " + id);
         Optional<AnnotacionDto> annotacion = annotationService.getById(id);
 

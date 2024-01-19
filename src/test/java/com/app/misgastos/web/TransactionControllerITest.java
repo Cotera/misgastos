283fe058c34898e32f1f84858cd0ab76d619ce77
CristianMausque <c.mausque@ibermatica.com>
2024-01-19 17:15:30 +0100

    test integracion

--- src/test/java/com/app/misgastos/web/TransactionControllerITest.java
+++ src/test/java/com/app/misgastos/web/TransactionControllerITest.java
@@ -12,15 +12,18 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -49,6 +52,24 @@ public class TransactionControllerITest {
     }
 
     @Test
+    public void getById() throws Exception {
+        Long givenId = 1L;
+
+        TransactionDto transactionDto = new TransactionDto();
+        transactionDto.setId(1L);
+        transactionDto.setDescription("transaction1");
+
+        when(transactionService.getById(givenId)).thenReturn(Optional.of(transactionDto));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/transaction/{id}", givenId))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.content().string(objectMapper.writeValueAsString(transactionDto)));
+
+    }
+
+    @Test
     public void getAll() throws Exception {
         TransactionDto transaction1 = new TransactionDto();
         transaction1.setId(1L);
@@ -66,6 +87,7 @@ public class TransactionControllerITest {
 
     @Test
     public void update() throws Exception {
+
         Long givenId = 1L;
         TransactionDto givenBody = new TransactionDto();
         givenBody.setDescription("transaction1");
